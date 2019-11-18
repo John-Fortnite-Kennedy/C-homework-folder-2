d57a101e5dbe8838355fd9d33dd34ffd8fa6743b
John-Fortnite-Kennedy <57903460+John-Fortnite-Kennedy@users.noreply.github.com>
2019-11-19 00:07:02 +0600

    Add files via upload

---
+++ 50.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+int x,y,z,a,u=0;
+cin>>x>>y>>z>>a;
+if (x==y){cout<<"yes";}
+if (x==z){cout<<"yes";}
+if (x==a){cout<<"yes";}
+if (y==z){cout<<"yes";}
+if (y==a){cout<<"yes";}
+if (z==a){cout<<"yes";}
+}
