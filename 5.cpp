08a6baa53ab2e981481f58bb32d3de69a53a8144
John-Fortnite-Kennedy <57903460+John-Fortnite-Kennedy@users.noreply.github.com>
2019-11-18 20:35:23 +0600

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<4;i++){
+          switch(i){
+        case 0:
+        cout<<"*     *     *"<<endl;
+        break;
+        case 1:
+        cout<<" *   * *   *"<<endl;
+        break;
+        case 2:
+        cout<<"  * *   * *"<<endl;
+        break;
+        case 3:
+        cout<<"   *     *"<<endl;
+        break;
+        }
+}
+}
