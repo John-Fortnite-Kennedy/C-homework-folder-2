a2af248790c3f3b3ebeccc1b695b06a18942d99a
John-Fortnite-Kennedy <57903460+John-Fortnite-Kennedy@users.noreply.github.com>
2019-11-18 23:15:50 +0600

    Add files via upload

---
+++ 35.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+int x;
+cin>>x;
+if (x<7){cout<<"Yes";}
+else if (x>10){cout<<"No";}
+else if (x==9){cout<<"Error";}
+cout<<x;
+}
