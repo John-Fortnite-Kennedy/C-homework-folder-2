25d50b61eaa31220bac7a351d2093f85ee81e552
John-Fortnite-Kennedy <57903460+John-Fortnite-Kennedy@users.noreply.github.com>
2019-11-18 21:21:13 +0600

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+double a=(-2.34);
+cout<<( ((sqrt(pow(a-5,2)) )-sin(a))/ 3)+sqrt(pow(a,2)+2014)*cos(2*a)-3;
+}
---
+++ 12.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+double a=3.6;
+cout<<exp(a-2)+sqrt(pow(sin(a),2))-pow(a,4)*cos(1/a);
+}
